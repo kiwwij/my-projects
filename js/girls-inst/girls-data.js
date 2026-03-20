2e219ea3320a5bafbe9eae7186bbe95f822fc13d
Takhtarov Serhii <104929300+kiwwij@users.noreply.github.com>
2026-03-20 09:57:23 +0200

    secret v11

--- js/girls-inst/girls-data.js
+++ js/girls-inst/girls-data.js
@@ -2,28 +2,28 @@ const categories = [
     {
         id: 'cat1',
         folder: 'girls-inst/ass', // Путь относительно html файла
-        count: 17,
+        count: 100,
         label: 'Ass',
         icon: 'bx-heart'
     },
     {
         id: 'cat2',
         folder: 'girls-inst/boobs',
-        count: 39,
+        count: 100,
         label: 'Boobs',
         icon: 'bx-heart-circle'
     },
     {
         id: 'cat3',
         folder: 'girls-inst/height',
-        count: 21,
+        count: 100,
         label: 'Height',
         icon: 'bx-star'
     },
     {
         id: 'cat4',
         folder: 'girls-inst/thighs',
-        count: 26,
+        count: 100,
         label: 'Thighs',
         icon: 'bx-female'
     }
